63a0aa9ed50bd6ec4911ea9bfe88823d490a6784
Admin <admin@Admins-Mac.local>
2014-08-25 15:54:42 +0400

    app with yellow storyboard

--- SimpleNotes/PSRMasterViewController.m
+++ SimpleNotes/PSRMasterViewController.m
@@ -28,15 +28,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
-    self.tableView.dataSource = self;
-    self.tableView.delegate = self;
+    //self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    //self.tableView.dataSource = self;
+    //self.tableView.delegate = self;
     
-    [self.view addSubview:self.tableView];
-    self.navigationItem.title = @"Notes";
+    //[self.view addSubview:self.tableView];
+    //self.navigationItem.title = @"Notes";
     
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNote)];
-    self.navigationItem.rightBarButtonItem = addButton;
+    //UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addNote)];
+    //self.navigationItem.rightBarButtonItem = addButton;
 }
 
 - (void)addNote {
